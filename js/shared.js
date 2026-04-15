8768199606495dc89f003f871c1af69e70c5cf73
zachjrt2 <167731525+zachjrt2@users.noreply.github.com>
2026-04-15 12:02:16 -0500

    Staff order randomization

--- js/shared.js
+++ js/shared.js
@@ -183,3 +183,16 @@ function initShared(activePage) {
   renderFooter();
   initScrollReveals();
 }
+
+/**
+ * Shuffles an array in-place using the Fisher-Yates algorithm.
+ * @param {Array} array 
+ * @returns {Array} The shuffled array
+ */
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
